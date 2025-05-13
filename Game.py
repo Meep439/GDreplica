8e7ccc6c3f4891f97eb8907f01adb98925ada3cd
Meep439 <nicoandmia@gmail.com>
2025-05-13 17:36:10 -0400

    Update Game.py

--- Game.py
+++ Game.py
@@ -6,7 +6,7 @@ pygame.init()
 
 # Screen dimensions
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 400
+SCREEN_HEIGHT = 390
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Geometry Dash Replica")
 
